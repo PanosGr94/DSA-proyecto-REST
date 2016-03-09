f62759d69dacb93e18e10b7839b95bc0c1e7bd7a
Panos Gr <nosgr94@gmail.com>
2016-03-09 18:05:35 +0100

    DAO added!!!

---
+++ service/src/test/java/edu/upc/eetac/dsa/beeter/dao/StingDAO.java
@@ -0,0 +1,18 @@
+package edu.upc.eetac.dsa.beeter.dao;
+
+import edu.upc.eetac.dsa.beeter.entity.Sting;
+import edu.upc.eetac.dsa.beeter.entity.StingCollection;
+
+import java.sql.SQLException;
+
+/**
+ * Created by Panos on 09-Mar-16.
+ */
+public interface StingDAO {
+    public Sting createSting(String userid, String subject, String content) throws SQLException;
+    public Sting getStingById(String id) throws SQLException;
+    public StingCollection getStings() throws SQLException;
+    public Sting updateSting(String id, String subject, String content) throws SQLException;
+    public boolean deleteSting(String id) throws SQLException;
+}
+
---
+++ service/src/test/java/edu/upc/eetac/dsa/beeter/dao/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package edu.upc.eetac.dsa.beeter.dao;
+
+/**
+ * Created by Panos on 08-Mar-16.
+ */
+public class UserAlreadyExistsException extends Throwable {
+
+}
